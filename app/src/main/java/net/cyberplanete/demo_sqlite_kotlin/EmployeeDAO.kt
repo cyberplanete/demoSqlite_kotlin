263762da42e4ea8272a73466ba8b42f8c7e7ffa6
pascal <pascal.joret@gmail.com>
2022-05-10 17:06:53 +0200

    Configuration pour la creation et le stockage des données

---
+++ app/src/main/java/net/cyberplanete/demo_sqlite_kotlin/EmployeeDAO.kt
@@ -0,0 +1,25 @@
+package net.cyberplanete.demo_sqlite_kotlin
+
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface EmployeeDAO {
+
+    @Insert
+    suspend fun insert(employeeEntity: EmployeeEntity)
+
+    @Update
+    suspend fun  update(employeeEntity: EmployeeEntity)
+
+    @Delete
+    suspend fun delete(employeeEntity: EmployeeEntity)
+
+    @Query("SELECT * FROM `employee-table`")
+    fun fetchAllEmployee():Flow<List<EmployeeEntity>>
+
+    @Query("SELECT * FROM `employee-table` where id=:id ")
+    fun fetchEmployeeByID(id:Int):Flow<List<EmployeeEntity>>
+
+
+}
