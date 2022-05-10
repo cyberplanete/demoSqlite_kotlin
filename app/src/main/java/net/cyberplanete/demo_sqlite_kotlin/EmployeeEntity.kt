82d7e73e37249bac2ab6e091a8f7b12a8602268e
pascal <pascal.joret@gmail.com>
2022-05-10 14:47:27 +0200

    Configuration d'une class data EmployeeEntity.kt

---
+++ app/src/main/java/net/cyberplanete/demo_sqlite_kotlin/EmployeeEntity.kt
@@ -0,0 +1,17 @@
+package net.cyberplanete.demo_sqlite_kotlin
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "employee-table")
+data class EmployeeEntity
+    (
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val name: String = "",
+    @ColumnInfo(name = "email-id")
+    val email: String = "",
+
+
+    )
